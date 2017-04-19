21d2006e9738595efa60a29b7fb59d640fb08f80
Michael Skiba <mskiba@Marjories-MacBook-Pro.local>
2017-04-19 16:23:53 -0400

    Initial Commit

---
+++ AnglePanGestureRecognizer/AnglePanGestureRecognizer.h
@@ -0,0 +1,19 @@
+//
+//  AnglePanGestureRecognizer.h
+//  AnglePanGestureRecognizer
+//
+//  Created by Michael Skiba on 4/19/17.
+//  Copyright © 2017 Atelier Clockwork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnglePanGestureRecognizer.
+FOUNDATION_EXPORT double AnglePanGestureRecognizerVersionNumber;
+
+//! Project version string for AnglePanGestureRecognizer.
+FOUNDATION_EXPORT const unsigned char AnglePanGestureRecognizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnglePanGestureRecognizer/PublicHeader.h>
+
+
