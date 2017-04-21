5b445a10ada51f836e1f51be64f358f8a846fa5e
Matt Buckley <matt.buckley@raizlabs.com>
2017-04-21 15:59:11 -0400

    Placate SwiftLint.

--- AnglePanGestureRecognizer/AnglePanGestureRecognizer.swift
+++ AnglePanGestureRecognizer/AnglePanGestureRecognizer.swift
@@ -11,22 +11,22 @@ import UIKit
 import UIKit
 
 private struct MovementVector {
-    
+
     /// The the distance moved
     var distance: CGFloat
-    
+
     /// The angle of motion in radians, expressed in clockwisse rotation from the 12 oclock position
     var angle: CGFloat
-    
+
     init(distance: CGFloat, angle: CGFloat) {
         self.distance = distance
         self.angle = angle
     }
-    
+
     init(point: CGPoint) {
         self = MovementVector(x: point.x, y: point.y)
     }
-    
+
     init(x: CGFloat, y: CGFloat) {
         self.distance = (x * x + y * y).squareRoot()
         guard self.distance > 0 else {
@@ -62,13 +62,13 @@ private struct MovementVector {
         }
         self.angle = asin(leg / self.distance) + offset
     }
-    
+
     var offset: CGPoint {
         let s1 = sin(self.angle) * self.distance
         let s2 = -cos(self.angle) * self.distance
         return CGPoint(x: s1, y: s2)
     }
-    
+
     mutating func converge(with vector: MovementVector) {
         let difference = MovementVector.differenceBetween(angle, and: vector.angle)
         if difference < CGFloat.pi / 2 {
@@ -80,11 +80,11 @@ private struct MovementVector {
         }
         angle = vector.angle
     }
-    
+
     func sortByDifference(lhs: CGFloat, rhs: CGFloat) -> Bool {
         return MovementVector.differenceBetween(angle, and: lhs) < MovementVector.differenceBetween(angle, and: rhs)
     }
-    
+
     private static func differenceBetween(_ angle1: CGFloat, and angle2: CGFloat) -> CGFloat {
         let high = max(angle1, angle2)
         let low = min(angle1, angle2)
@@ -97,25 +97,25 @@ private struct MovementVector {
         }
         return diff
     }
-    
+
 }
 
 class AnglePanGestureRecognizer: UIPanGestureRecognizer {
-    
+
     var allowedAngles: [CGFloat] = []
     var maxAngleDifference: CGFloat = CGFloat.pi / 6
     var unlockedMoveDistance: CGFloat = 5
     var moveDistance: CGFloat?
-    
+
     fileprivate(set) var currentAngle: CGFloat?
-    
+
     var finalOffset: CGPoint? {
         guard let angle = currentAngle, let distance = moveDistance else {
             return nil
         }
         return MovementVector(distance: distance, angle: angle).offset
     }
-    
+
     func percentComplete(in view: UIView?) -> CGFloat {
         guard let maxDistance = moveDistance else {
             return 0
@@ -123,19 +123,19 @@ class AnglePanGestureRecognizer: UIPanGestureRecognizer {
         let currentDistance = adjustedVector(in: view).distance
         return min(currentDistance / maxDistance, 1)
     }
-    
+
     func reset() {
         currentAngle = nil
     }
-    
+
     func adjustedTranslation(in view: UIView?) -> CGPoint {
         return adjustedVector(in: view).offset
     }
-    
+
 }
 
 private extension AnglePanGestureRecognizer {
-    
+
     func adjustedVector(in view: UIView?) -> MovementVector {
         let initialVector = MovementVector(point: translation(in: view))
         if initialVector.distance >= unlockedMoveDistance && currentAngle == nil {
@@ -157,6 +157,5 @@ private extension AnglePanGestureRecognizer {
         }
         return adjustedVector
     }
-    
-}
 
+}
